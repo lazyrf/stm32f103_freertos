7d463e39eb723c9d1d3d59b455f3c338d70580d6
Aron_Li <lazyrf.tw@gmail.com>
2019-12-17 17:09:08 +0800

    First app

--- app/main.c
+++ app/main.c
@@ -1,4 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "config.h"
+#include "bsp_rcc.h"
+#include "bsp_led.h"
+#include "bsp_dbg_uart.h"
+#include "delay.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+void task1(void *arg)
+{
+	while (1) {
+		printf("Hello world\r\n");
+		vTaskDelay(1000);
+	}
+}
+
+void task2(void *arg)
+{
+	while (1) {
+		LED_TOGGLE(CONFIG_LED_1_GPIO_PORT, CONFIG_LED_1_GPIO_PIN);
+		vTaskDelay(500);
+	}
+}
+
+void init_task(void *arg)
+{
+	printf("Create task1...\r\n");
+	xTaskCreate(task1, "Task1", 64, NULL, 2, NULL);
+
+	printf("Create task2...\r\n");
+	xTaskCreate(task2, "Task2", 64, NULL, 2, NULL);
+
+	vTaskDelete(NULL);
+}
+
 int main(void)
 {
-	return 0;
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
+
+	bsp_rcc_init();
+	bsp_led_init();
+	bsp_dbg_uart_init();
+
+	xTaskCreate(init_task, "Init Task", 64, NULL, 3, NULL);
+	vTaskStartScheduler();
 }
